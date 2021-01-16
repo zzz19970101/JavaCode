fdceadf03cb1dd00cb6a2305cc3839ac26d7947a
张先森 <317541175@qq.com>
2021-01-16 08:56:32 +0800

    Java项目2（添加客户无上限）

---
+++ src/com/zzz/service/CustomerList.java
@@ -0,0 +1,134 @@
+package com.zzz.service;
+
+import com.zzz.bean.Customer;
+
+import java.util.ArrayList;
+
+/**
+ * 多个客户构成的数组
+ * 提供对客户的增删改查的方法
+ *
+ * @author zzz
+ * @create 2021-01-14 10:52
+ */
+public class CustomerList {
+    //    private Customer [] customers;
+    private ArrayList<Customer> custs;
+//    private int total = 0;
+
+//    public CustomerList() {
+//    }
+
+    public CustomerList() {
+//        customers = new Customer[totalCustomer];
+        custs = new ArrayList<>();
+        Customer originCustomer = new Customer("zzz", '男', 18, "13000000000", "zzz@gmail.com");
+//        customers[0] = originCustomer;
+//        total++;
+        custs.add(originCustomer);
+    }
+
+    /**
+     * 此方法为增加一个客户
+     *
+     * @param customer
+     * @return 无返回值
+     */
+    public void addCustomer(Customer customer) {
+//        if(total >= customers.length){
+//            return false;
+//        }else{
+//            customers[total] = customer;
+//            total++;
+//            return true;
+//        }
+        custs.add(customer);
+    }
+
+    /**
+     * 修改一个索引为index的客户信息
+     *
+     * @param index 所要修改的客户的编号
+     * @param cust  需要替换的客户
+     * @return 修改成功返回true；失败返回false。
+     */
+    public boolean repalceCustomer(int index, Customer cust) {
+        if (index <= 0 || index > custs.size()) {
+            return false;
+        } else {
+            custs.set(index - 1, cust);
+            return true;
+        }
+    }
+
+    /**
+     * 删除一个索引为index的客户
+     *
+     * @param index 客户的索引
+     * @return 当删除成功时，返回true；删除失败，返回false。
+     */
+    public boolean deleteCustomer(int index) {
+        if (index <= 0 || index > custs.size()) {
+            return false;
+        } else {
+//            for (int i = index; i < total; i++) {
+//                customers[i - 1] = customers[i];
+//            }
+//            customers[total - 1] = null;
+//            total --;
+            custs.remove(index - 1);
+            return true;
+        }
+    }
+
+    /**
+     * 得到客户信息数组
+     *
+     * @return 返回一个Customer类型的集合
+     */
+    public ArrayList<Customer> getAllCustmors() {
+//        Customer custs[] = new Customer[total];
+//        for (int i = 0; i <= total - 1; i++) {
+//            custs[i] = customers[i];
+//        }
+//        return custs;
+        return custs;
+
+    }
+
+    /**
+     * 返回指定索引的客户对象
+     *
+     * @param index 需要返回的客户信息索引
+     * @return 一个索引为index的Customer对象，当index不存在时，返回null。
+     */
+    public Customer getCustomer(int index) {
+        if (index <= 0 || index > custs.size()) {
+            return null;
+        } else {
+//            return customers[index - 1];
+            return custs.get(index - 1);
+        }
+    }
+
+    /**
+     * 得到数组中元素的个数
+     *
+     * @return 以int型返回数组中元素的个数
+     */
+    public int getSize() {
+        return custs.size();
+    }
+
+    /*下面是测试代码，不用理会*/
+//    public static void main(String[] args) {
+//        CustomerList list = new CustomerList(5);
+//        Customer customer1 = new Customer("Tom", '男', 23, "12345", "Tom@emial.com");
+//        boolean b = list.addCustomer(customer1);
+//        System.out.println(b);
+//        boolean b1 = list.deleteCustomer(1);
+//        System.out.println(b1);
+//        boolean b2 = list.deleteCustomer(1);
+//        System.out.println(b2);
+//    }
+}
